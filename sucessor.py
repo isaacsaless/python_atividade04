1ec484aa19a6165acb37655be23a9ee1a62eb380
isaacsaless <isaacnascimentosales@gmail.com>
2023-10-11 11:09:25 -0300

    Primeira versão do código de sucessor e antecessor

---
+++ sucessor.py
@@ -0,0 +1,7 @@
+# Escreva um numero e veja qual o seu antecessor e seu sucessor!
+
+print('Descubra o sucessor e o antecessor de um número qualquer')
+numero = float(input('Digite um núemro: '))
+sucessor = numero+1
+antecessor = numero-1
+print(f'O sucessor do seu número é {sucessor} e o antecessor é {antecessor}.')
